0f4076e932ba731823b4b4b14f8b648420a3f67e
Jacky Chen <Jacky.Chen@quest.com>
2021-03-19 16:52:00 +0800

    init project

---
+++ src/main/java/com/jchen/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.jchen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.jchen.*"})
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
